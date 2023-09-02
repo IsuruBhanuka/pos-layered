e6fd546c837131d72df85f49c29722cb3bd0b3be
IsuruBhanuka <isuru.1234bhanu@gmail.com>
2023-09-02 17:56:59 +0530

    Customer service impl

--- src/pos/layered/service/customer/impl/CustomerServiceImpl.java
+++ src/pos/layered/service/customer/impl/CustomerServiceImpl.java
@@ -4,7 +4,10 @@
  */
 package pos.layered.service.customer.impl;
 
+import pos.layered.dao.DaoFactory;
+import pos.layered.dao.customer.CustomerDao;
 import pos.layered.dto.CustomerDto;
+import pos.layered.entity.CustomerEntity;
 import pos.layered.service.customer.CustomerService;
 
 /**
@@ -12,10 +15,27 @@ import pos.layered.service.customer.CustomerService;
  * @author BHANUKA
  */
 public class CustomerServiceImpl implements CustomerService {
+    
+    CustomerDao customerDao = (CustomerDao) DaoFactory.getInstance().getDao(DaoFactory.DaoTypes.CUSTOMER);
 
-    @Override
+ 
     public String saveCustomer(CustomerDto customerDto) throws Exception {
-        return null;
+        CustomerEntity customerEntity = new CustomerEntity(
+                customerDto.getId(), 
+                customerDto.getTitle(), 
+                customerDto.getName(), 
+                customerDto.getAdress(), 
+                customerDto.getDob(), 
+                customerDto.getSalary(), 
+                customerDto.getCity(), 
+                customerDto.getProvince(), 
+                customerDto.getZip());
+        
+        if (customerDao.saveCustomer(customerEntity)) {
+            return "Successfully Added";
+        } else {
+            return "Fail";
+        }
     }
     
 }
